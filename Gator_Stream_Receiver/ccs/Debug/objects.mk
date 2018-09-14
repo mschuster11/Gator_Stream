27aa9d96b8594bbae0b35e3f65bf0283369e693f
mschuster11 <mschuster11@ufl.edu>
2018-09-14 01:24:59 -0400

    Cleaning up repo

--- Gator_Stream_Receiver/ccs/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -lBluetopia_BR_EDR.lib -lSS1BTAUD.lib -lSS1BTAVC.lib -lSS1BTAVR.lib -lSS1BTGAV.lib
-
